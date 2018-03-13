2219ea0ea924e8d6f572373d5bd7928494704a24
forevermistery <1258234227@qq.com>
2018-03-13 19:32:23 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu02/SplashActivity.java
@@ -0,0 +1,52 @@
+package cn.edu.gdmec.android.boxuegu02;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by apple on 18/3/13.
+ */
+
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView tv_version;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+        tv_version = (TextView) findViewById(R.id.tv_version);
+        try{
+
+            PackageInfo info=getPackageManager().getPackageInfo(getPackageName(),0);
+          tv_version.setText("V"+info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            tv_version.setText("V");
+        }
+
+        Timer timer=new Timer();
+        TimerTask task=new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                SplashActivity.this.finish();
+
+            }
+        };
+        timer.schedule(task,3000);
+
+    }
+
+
+}
